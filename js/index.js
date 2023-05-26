fce9b77c1e65561663d4a8b49c4d6849fdce041f
Oleg Sinitsin <wg123@yandex.ru>
2023-05-26 17:08:54 +0500

    fourth commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// берем в константы переменные для скролла
+// переменные для скролла
 const header = document.querySelector(".header");
 
 // функциря скролла
@@ -9,3 +9,29 @@ window.addEventListener("scroll", function () {
     header.classList.remove("header__background");
   }
 });
+
+// переменные для перевода языка
+const select = document.querySelector(".headaer__language-select");
+
+// функция изменения url
+select.addEventListener("change", function () {
+  let lang = select.value;
+  // '"#" + lang' - это hash
+  location.href = window.location.pathname + "#" + lang;
+  location.reload();
+});
+// функция изменения языка
+function changeLang() {
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  select.value = hash;
+  // перебор классов
+  for (let key in arrLang) {
+    let elem = document.querySelector(".lang-" + key);
+    if (elem) {
+      elem.innerHTML = arrLang[key][hash];
+    }
+  }
+}
+
+changeLang();
---
+++ js/lang.js
@@ -0,0 +1,229 @@
+const arrLang = {
+  // header
+  main: {
+    es: "home",
+    en: "home",
+    ru: "На главную",
+  },
+  rooms: {
+    es: "Habitaciones",
+    en: "Rooms",
+    ru: "Номера",
+  },
+  events: {
+    es: "Eventos",
+    en: "Events",
+    ru: "События",
+  },
+  contact: {
+    es: "Contacto",
+    en: "Contacts",
+    ru: "Контакты",
+  },
+  // home
+  home: {
+    es: "Vive una experiencia única en nuestros eventos multitudinarios",
+    en: "Live a unique experience in our massive events",
+    ru: "Почувствуйте уникальный опыт наших массовых мероприятий",
+  },
+  video: {
+    es: "ver video",
+    en: "watch video",
+    ru: "cмотреть видео",
+  },
+  //bookingStrip
+  dataArrival: {
+    es: "Fecha entrada",
+    en: "Arrival date",
+    ru: "Дата прибытия",
+  },
+  dataDeparture: {
+    es: "Fecha salida",
+    en: "Departure date",
+    ru: "Дата отбытия",
+  },
+  roomsFor: {
+    es: "Cuartos para",
+    en: "Rooms for",
+    ru: "Комнаты для",
+  },
+  personOne: {
+    es: "1 persona",
+    en: "1 persons",
+    ru: "1 человека",
+  },
+  personTwo: {
+    es: "2 personas",
+    en: "2 persons",
+    ru: "2 человека",
+  },
+  personThree: {
+    es: "3 personas",
+    en: "3 persons",
+    ru: "3 человека",
+  },
+  reserve: {
+    es: "Reservar ahora",
+    en: "Reserve now",
+    ru: "Забронировать",
+  },
+  //events
+  enjoy: {
+    es: "¡Disfruta con nosotros!",
+    en: "Enjoy with us!",
+    ru: "Наслаждайтесь вместе с нами!",
+  },
+  kindsEvents: {
+    es: "Realizamos todo tipo de eventos",
+    en: "We do all kinds of events",
+    ru: "Делаем все виды мероприятий",
+  },
+  in: {
+    es: "en",
+    en: "in",
+    ru: "в",
+  },
+  intwo: {
+    es: "en",
+    en: "in",
+    ru: "в",
+  },
+  northeastBeach: {
+    es: "Playa Noreste",
+    en: "northeast beach",
+    ru: "северо-восточный пляж",
+  },
+  tomorrowlandBeach: {
+    es: "Tomorrowland Beach",
+    en: "Tomorrowland Beach",
+    ru: "Пляж",
+  },
+  text: {
+    es: "Breve descripción del evento que puede ocupara hasta 3 líneas de texto.",
+    en: "Brief description of the event that can occupy up to 3 lines of text.",
+    ru: "Краткое описание события, которое может занимать до 3-х строк текста.",
+  },
+  textTwo: {
+    es: "Breve descripción del evento que puede ocupara hasta 3 líneas de texto.",
+    en: "Brief description of the event that can occupy up to 3 lines of text.",
+    ru: "Краткое описание события, которое может занимать до 3-х строк текста.",
+  },
+  interested: {
+    es: "Me interesa",
+    en: "I'm interested",
+    ru: "Интересно",
+  },
+  interestedTwo: {
+    es: "Me interesa",
+    en: "I'm interested",
+    ru: "Интересно",
+  },
+  southBeach: {
+    es: "Playa Sur",
+    en: "South Beach",
+    ru: "Южный пляж",
+  },
+  worldTropical: {
+    es: "World Tropical",
+    en: "Tropical World",
+    ru: "Тропический мир",
+  },
+  //reserve
+  experience: {
+    es: "Una super experiencia para contar",
+    en: "A super experience to tell",
+    ru: "Супер опыт, чтобы рассказать",
+  },
+  reserveTwo: {
+    es: "Reservar ahora",
+    en: "Reserve now",
+    ru: "Забронировать",
+  },
+  //price
+  luxury: {
+    es: "Reservar ahora",
+    en: "Reserve now",
+    ru: "Роскошные номера",
+  },
+  catalog: {
+    es: "Vea todo nuestro catálogo de habitaciones",
+    en: "See our entire catalog of rooms",
+    ru: "Смотрите весь наш каталог номеров",
+  },
+  Wilderness: {
+    es: "Wilderness Club at Big Ceddar",
+    en: "Wilderness Club at Big Ceddar",
+    ru: "Клуб дикой природы в Большом Сиддаре",
+  },
+  WildernessTwo: {
+    es: "Wilderness Club at Big Ceddar",
+    en: "Wilderness Club at Big Ceddar",
+    ru: "Клуб дикой природы в Большом Сиддаре",
+  },
+  WildernessThree: {
+    es: "Wilderness Club at Big Ceddar",
+    en: "Wilderness Club at Big Ceddar",
+    ru: "Клуб дикой природы в Большом Сиддаре",
+  },
+  Bathrooms: {
+    es: "2 Baños",
+    en: "2 bathrooms",
+    ru: "2 ванные комнаты",
+  },
+  nigth: {
+    es: "3 night",
+    en: "3 night",
+    ru: "3 ночей",
+  },
+  nigthTwo: {
+    es: "6 night",
+    en: "6 night",
+    ru: "6 ночей",
+  },
+  nigthTrhee: {
+    es: "8 night",
+    en: "8 night",
+    ru: "8 ночей",
+  },
+  jacuzzi: {
+    es: "1 Baño con jacujacuzzi",
+    en: "1 bathroom with jacuzzi",
+    ru: "1 ванная комната с джакузи",
+  },
+  Matrimonial: {
+    es: "Habitación matrimonial",
+    en: "Matrimonial room",
+    ru: "Супружеская комната",
+  },
+  //footer
+  hotelDescription: {
+    es: "Descripción del hotel. Detalles que los hagan especial",
+    en: "Hotel description. Details that make them special",
+    ru: "Описание отеля. Детали, которые делают их особенными",
+  },
+  mainTwo: {
+    es: "home",
+    en: "home",
+    ru: "На главную",
+  },
+  roomsTwo: {
+    es: "Habitaciones",
+    en: "Rooms",
+    ru: "Номера",
+  },
+  eventsTwo: {
+    es: "Eventos",
+    en: "Events",
+    ru: "События",
+  },
+  logo: {
+    es: "© Logo Hotel 2023. Derechos reservados",
+    en: "© Logo Hotel 2023. All rights reserved",
+    ru: "© Logo Hotel 2023. Все права защищены.",
+  },
+  Find: {
+    es: "Encuentranos en:",
+    en: "Find us in:",
+    ru: "Найдите нас в:",
+  },
+};
